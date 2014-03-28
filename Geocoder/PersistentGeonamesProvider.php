6ad2aa3056e5681b871af876e40945524d4a8aba
Rafael Calleja <rafa.calleja@d-noise.net>
2014-03-28 12:14:26 +0100

    added cache query and population order

--- Geocoder/PersistentGeonamesProvider.php
+++ Geocoder/PersistentGeonamesProvider.php
@@ -73,17 +73,29 @@ class PersistentGeonamesProvider implements LocaleAwareProviderInterface
         $dql = <<<DQL
 SELECT t
 FROM Giosh94mhzGeonamesBundle:Toponym t
-WHERE t.name LIKE :name
+WHERE t.name LIKE :name order by t.population desc
 DQL;
 
         /* @var $query \Doctrine\ORM\Query */
         $query = $this->om->createQuery($dql);
+
+        //Cache
+        $key = __METHOD__. serialize($query->getParameters());
+        $time = 600;
+
+        $query->useResultCache(true, $time, $key);
+        $query->useQueryCache(true);
+
+        //$query->setCacheable(true);
+        //$newkey = md5(__METHOD__. serialize($query->getParameters())."offset=".$query->getFirstResult()."length=".$query->getMaxResults().serialize($query->getHints()).$query->getDQL());
+        //$query->setHydrationCacheProfile(new QueryCacheProfile($time, $newkey));
 
         $toponyms = $query
             ->setParameter('name', "%{$address}%")
             ->setMaxResults($this->maxResults)
             ->execute()
         ;
+
         if (empty($toponyms))
             throw new NoResultException();
 
